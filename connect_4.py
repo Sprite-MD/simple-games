2bab82bf48467359dd11e30059f94381202468f1
Sprite <ponkaew.wasin@gmail.com>
2021-05-15 20:19:57 -0500

    attempted check for 4 in a row, still WIP

--- connect_4.py
+++ connect_4.py
@@ -10,24 +10,76 @@ board = create_board()
 
 play = True
 
-def connect4(arr):
-    for row in range(6):
+def connect4(board):
+    
+    count1 = 0
+    count2 = 0
+    # Check horizontal
+    while count1 != 4 or count2 != 4:
+        for row in range(6):
+            for column in range(7):
+                if board[row][column] == 1:
+                    count1 += 1
+                    count2 = 0
+                    if count1 == 4:
+                        return True
+                elif board[row][column] == 2:
+                    count2 += 1
+                    count1 = 0
+                    if count2 == 4:
+                        return True
+    
+    # Check Vertical
+    while count1 != 4 or count2 != 4:
+        for column in range(6):
+            for row in range(7):
+                if board[row][column] == 1:
+                    count1 += 1
+                    count2 = 0
+                    if count1 == 4:
+                        return True
+                elif board[row][column] == 2:
+                    count2 += 1
+                    count1 = 0
+                    if count2 == 4:
+                        return True
+
+
 
 while play:
+    
 
     player_1 = int(input('Please pick a column: 1 - 7 \n'))
     for row in range(5, 0, -1):
-        if board[row][player_1] == 0:
-            board[row][player_1] = 'X'
+        if board[row][player_1 - 1] == 0:
+            board[row][player_1 - 1] = 1
+
             break
+
     print(board)
+
+    if connect4(board) == True:
+        print('Congrats! Player 1 Wins!')
+        break
+
+
+    
+
+
 
 
 
     player_2 = int(input('Please pick a column: 1 - 7 \n'))
     for row in range(5, -1, -1):
-        if board[row][player_1] == 0:
-            board[row][player_1] = 'O'
+        if board[row][player_2 - 1] == 0:
+            board[row][player_2 - 1] = 2
             break
+
     print(board)
+
+    if connect4(board) == True:
+        print('Congrats! Player 2 Wins!')
+        break
+    
+
 
